740afe86d289e09b5e2ef2a9b9c50bd85d3d499b
azabel <azabel@locktonaffinity.com>
2017-06-27 14:10:34 -0500

    completed testing setup

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,3 +1,5 @@
 import chalk from "chalk";
 
 console.log(chalk.green("Starting app in dev mode...")); // eslint-disable-line no-console
+const a = 100;
+console.log(a); // eslint-disable-line no-console
